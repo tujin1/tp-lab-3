9d8ad6bd0b4e772264dcf65673b2bb55547567cb
tujin1 <44016577+tujin1@users.noreply.github.com>
2019-03-20 11:56:57 +0300

    Update Circle.cpp

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -7,7 +7,7 @@ Circle::Circle(double r)
 	radius = r;
 	ference = 2 * pi*r;
 	area = pi * r*r;
-}
+};
 
 void  Circle::setRadius(double r)
 {
@@ -15,33 +15,33 @@ void  Circle::setRadius(double r)
 	ference = 2 * pi*r;
 	area = pi * r*r;
 
-}
+};
 
 void  Circle::setFerence(double f)
 {
 	ference = f;
 	radius = f / (2 * pi);
 	area = f*f / (4 * pi);
-}
+};
 
 void  Circle::setArea(double a)
 {
 	area = a;
 	radius = sqrt(a / pi);
 	ference = 2 * pi*radius;
-}
+};
 
 double Circle::getRadius()
 {
 	return radius;
-}
+};
 
 double  Circle::getFerence()
 {
 	return ference;
-}
+};
 
 double  Circle::getArea()
 {
 	return area;
-}
+};
